f85e8a5d791a6f74126f8e2882c1c6199bde8dbe
noorahmedds <noorahmedds@gmail.com>
2025-09-15 11:03:28 +0200

    Tar utils and multi hop ssh commands adde

--- shell_utils/tar_utils.sh
+++ shell_utils/tar_utils.sh
@@ -1,2 +1,6 @@
 tar -cf - <folder> | pigz -9 > <folder>.tar.gz 
 pigz -dc ${SUPERDEC_STORAGE}/datasets/shapenet.tar.gz | tar -xf - -C ./
+
+# Zipping
+zip -r folders.zip folder_1
+unzip folders.zip
---
+++ slurm_utils/multi_hop_ssh.sh
@@ -0,0 +1,4 @@
+# Ran from local. Already have ran the server on an salloc node
+ssh -L 9999:<h12-07/compute-node-name>:9999 b266be11@<cluster_name/host_name>
+
+# Note: VSCode automatically forwards a port to your local machine which is pretty annoying an doesn't work. Delete the forwarded node from the "PORTS" tab when you run in vscode shell
